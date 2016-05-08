5c5fecb0e2ba61f84f893bd9e6a801da2940d0d8
Rohan Desai <rdesai@rohans-macbook-pro.local>
2016-05-08 01:12:42 -0700

    Initial Commit

---
+++ photosharing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photosharing
+//
+//  Created by Rohan Desai on 5/8/16.
+//  Copyright © 2016 rd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
