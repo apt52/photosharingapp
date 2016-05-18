725c26b25187ccdf8aff7c396bc944b1438f57eb
Rohan Desai <rohand.rl@gmail.com>
2016-05-17 18:30:48 -0700

    signup done with parse

--- photosharing/ViewController.swift
+++ photosharing/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import Parse
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var phoneNumber: UITextField!
@@ -16,15 +18,59 @@ class ViewController: UIViewController {
     @IBOutlet weak var emailId: UITextField!
     @IBOutlet weak var password: UITextField!
     
-    @IBAction func signUp(sender: AnyObject) {
-        if firstName.text == "" || lastName.text == "" || phoneNumber.text == "" || emailId.text == "" || password.text == "" {
-            var alert =  UIAlertController(title: "Error in form.", message: "Please enter all the details.", preferredStyle: UIAlertControllerStyle.Alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
-                self.dismissViewControllerAnimated(true, completion: nil)
-            }))
-            self.presentViewController(alert, animated: true, completion: nil)
-        }
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
     
+    func displayAlert(title: String, message: String) {
+        
+        let alert =  UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func signUp(sender: AnyObject) {
+        
+        if firstName.text == "" || lastName.text == "" || phoneNumber.text == "" || emailId.text == "" || password.text == "" {
+        
+            self.displayAlert("Error in form.", message: "Please enter all the details.")
+        
+        } else {
+            activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            activityIndicator.center = self.view.center
+            activityIndicator.hidesWhenStopped = true
+            activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.Gray
+            view.addSubview(activityIndicator)
+            activityIndicator.startAnimating()
+            UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+            
+            var user = PFUser()
+            var errorMessage = "Please try again later."
+            
+            user.username = firstName.text
+            user.email = emailId.text
+            user.password = password.text
+            user["firstName"] = firstName.text
+            user["lastName"] = lastName.text
+            user["phoneNumber"] = phoneNumber.text
+            
+            user.signUpInBackgroundWithBlock({ (success, error) in
+                if error == nil {
+                    self.activityIndicator.stopAnimating()
+                    UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                    self.displayAlert("Success!", message: "User signed up successfully!")
+                } else {
+                    if let errorString = error?.userInfo["error"] as? String {
+                        errorMessage = errorString
+                    }
+                    self.activityIndicator.stopAnimating()
+                    UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                    self.displayAlert("Signup error", message: errorMessage)
+                }
+            })
+        }
     }
     
     override func viewDidLoad() {
